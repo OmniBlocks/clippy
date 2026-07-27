3a7942af298dcb90613add950c122221934a783d
supervoidcoder <88671013+supervoidcoder@users.noreply.github.com>
2026-07-27 01:36:13 -0400

    Fix discussions page link in README generation
    Updated the link to the discussions page in the README text.

--- src/init.js
+++ src/init.js
@@ -137,7 +137,7 @@ export async function init() {
   const readmetext = `# Clippy
 A sample clippy extension.
 
-Check the [docs](https://omniblocks.github.io/clippy/) to learn more. Get help on our [discussions page](https://github.com/OmniBlocks/clippy/discussions).
+Check the [docs](https://omniblocks.github.io/clippy/) to learn more. Get help on our [discussions page](https://github.com/orgs/OmniBlocks/discussions).
 
 To contribute, you need to install Clippy. To do this, see https://omniblocks.github.io/clippy/tutorial/`
   fs.writeFileSync(path.join(process.cwd(), 'README.md'), readmetext)
